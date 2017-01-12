b344100c320ffe6ba8f3eed2fd177579edc239a3
Charles Piguet <cpiguet@msn.com>
2017-01-12 11:56:40 +0100

    foreach liste.js

--- app/models/liste.js
+++ app/models/liste.js
@@ -6,13 +6,18 @@ angular.module("liste").service("Liste",
 			{
 				this.id = data.id;
 				this.label = data.label;
+				this.todos = [];
 			}
 			
 			Liste.prototype.show = function()
 			{
 				$http.get("http://todos.api.netlor.fr/lists/" + this.id + "/todos", { headers : {'Authorization' : 'Token token=0cbd83dabea346dab268bf13ce476ae1'} }).then(function(response)
 				{
-					console.log(response);
+					response.data.forEach(function(data)
+					{
+						var newListe = new Liste(data);
+						$scope.tab.push(newListe);
+					});
 				},function(error)
 				{
 					console.log(error);
