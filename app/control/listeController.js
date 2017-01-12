992e09dc509931f99a186b9ff58ee0a4a79ee997
Charles Piguet <cpiguet@msn.com>
2017-01-12 11:13:31 +0100

    GET /lists fonctionnel
    Ca marche putain ! .

--- app/control/listeController.js
+++ app/control/listeController.js
@@ -1,16 +1,16 @@
 angular.module("liste")
 	.controller("ListeController", ["$scope", "$http", "Liste",
-	function($scope, $http, Product)
+	function($scope, $http, Liste)
 	{
 		$http.get("http://todos.api.netlor.fr/lists", { headers : {'Authorization' : 'Token token=0cbd83dabea346dab268bf13ce476ae1'} }).then(function(response)
 		{
-			console.log(response);
-			/*$scope.liste = [];
+			$scope.tab = [];
+			
 			response.data.forEach(function(data)
 			{
 				var newListe = new Liste(data);
-				$scope.liste.push(newProduct);
-			});*/
+				$scope.tab.push(newListe);
+			});
 		},
 		function(error)
 		{
