a323a65ede8dabb0550b4dfab91688ae6eb464c0
hitoshi54 <dylanclement7@gmail.com>
2017-01-16 11:30:01 +0100

    petite chose

--- app/models/tache.js
+++ app/models/tache.js
@@ -9,18 +9,18 @@ angular.module("liste").service("Tache",
 				this.fini = data.done;
 				this.liste = data.list_id.$oid;
 			}
-			
+
 			Tache.prototype.done = function()
 			{
 				console.log("http://todos.api.netlor.fr/lists/" + this.liste + "/todos/" + this.id + "/done");
 				console.log(this.fini);
-				
+
 				if(this.fini == false)
 				{
 					this.fini = true;
 					$http.put("http://todos.api.netlor.fr/lists/" + this.liste + "/todos/" + this.id + "/done", { headers : {'Authorization' : 'Token token=0cbd83dabea346dab268bf13ce476ae1'} }).then(function(response)
 					{
-						
+
 					},function(error)
 					{
 						console.log(error);
@@ -31,27 +31,26 @@ angular.module("liste").service("Tache",
 					this.fini = false;
 					$http.put("http://todos.api.netlor.fr/lists/" + this.liste + "/todos/" + this.id + "/undone", { headers : {'Authorization' : 'Token token=0cbd83dabea346dab268bf13ce476ae1'} }).then(function(response)
 					{
-						
+
 					},function(error)
 					{
 						console.log(error);
 					});
 				}
 			}
-			
+
 			Tache.prototype.modifTask = function()
 			{
 				$http.put("http://todos.api.netlor.fr/lists/" + this.liste + "/todos/" + this.id , '{"text": "' + document.getElementById("tache-" + this.id).value + '"}' ,{ headers : {'Authorization' : 'Token token=0cbd83dabea346dab268bf13ce476ae1'} }).then(function(response)
 				{
-					console.log(response);
 				},function(error)
 				{
 					console.log(error);
 				});
-				
+
 				this.text = document.getElementById("tache-" + this.id).value;
 			}
-			
+
 			return Tache;
 		}
 ]);
